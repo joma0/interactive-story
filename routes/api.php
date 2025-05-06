511e3500a9d1fd930f2ef8562f3a79b5ff0a6792
joma0 <joanah.mayor@heig-vd.ch>
2025-05-06 11:25:51 +0200

    création des routes api

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\StoryController;
+use App\Http\Controllers\ChapterController;
+use App\Http\Controllers\ChoiceController;
+
+Route::apiResource('stories', StoryController::class);
+
+Route::apiResource('chapters', ChapterController::class);
+
+Route::apiResource('choices', ChoiceController::class);
+
+Route::get('stories/{story}/chapters', function (App\Models\Story $story) {
+    return response()->json($story->chapters);
+});
+
+Route::get('chapters/{chapter}/choices', function (\App\Models\Chapter $chapter) {
+    return response()->json($chapter->originChoices);
+});
