2a33ff044a27dc31a911131d9df9897f8d078d76
joma0 <joanah.mayor@heig-vd.ch>
2025-05-10 05:59:31 +0200

    correction des erreurs pour les routes

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\StoryController;
 use App\Http\Controllers\ChapterController;
 use App\Http\Controllers\ChoiceController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::apiResource('stories', StoryController::class);
 
